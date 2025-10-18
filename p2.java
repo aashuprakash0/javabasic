e72cb3788fae531f703216763d9e5c67789ded9a
Aashu Prakash <156689800+aashuprakash0@users.noreply.github.com>
2025-10-18 23:24:38 +0530

    scanner programs

--- p2.java
+++ p2.java
@@ -1,14 +1,14 @@
-package programs;
-//Write a program to print odd number from 1 to 100 without using any loop
+package Scanner_programs;
+
+import java.util.Scanner;
+
 public class p2 {
-static int num=1; 
-public static void main(String[] args) {
-	if (num<=100) {
-		if(num%2 !=0) {
-		System.out.println(num);
-		}
-		num++; 
-		main(null);
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the String Sentence");
+		String l = sc.nextLine();
+		System.out.println("-----------------");
+		System.out.println(l);
 	}
-}
+
 }
