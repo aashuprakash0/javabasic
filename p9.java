4c36a3a40401585f17c311b52598b49e1aa6e752
Aashu Prakash <156689800+aashuprakash0@users.noreply.github.com>
2025-10-27 23:23:47 +0530

    Add files via upload

--- p9.java
+++ p9.java
@@ -1,27 +1,23 @@
 package pattern;
 /*WAP TO PRINT
-12345
-67891
-23456
-78912
-34567
+55555
+44444
+33333
+22222
+11111
 */
 
-public class p11 {
+public class p9 {
 	public static void main(String[] args) {
 		int row=5;
 		int coloumn=5; 
-		int num= 1;
+		int num= 5;
 	for (int i = 0; i < row; i++) {
 		for (int j = 0; j < coloumn; j++) {
-			System.out.print(num++);
-			if(num==10) {
-				num=1;	
-		}}
+			System.out.print(num);
+		}
 		System.out.println(); 
-		
+		num--;
 	}
 	}
-	}
-
-
+}
